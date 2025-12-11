05f8e9da448fa301ecf9933717e5e2d453f95a22
tebicap <tebicap@gmail.com>
2025-12-10 21:58:57 -0300

    actualizo serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,7 @@
-CACHE_NAME = 'v2'; // actualizar nro cada vez que actualizo otros archivos para que los recargue
+CACHE_NAME = 'v3'; // actualizar nro cada vez que actualizo otros archivos para que los recargue
 
 const ASSETS = [
+        './',
         './index.html',
         './estilos.css',
         './scripts.js',
@@ -37,7 +38,7 @@ const PRECACHE_URLS = new Set(ASSETS); // ← Set es mucho más rápido para bú
 // FETCH
 self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    event.respondWith(fetch(event.request).catch(() => caches.match('./')));
     return;
   }
 
